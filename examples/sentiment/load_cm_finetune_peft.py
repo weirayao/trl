dffe522e01d584ec526711e19e388a5c76b6139e
Edward Beeching <edbeeching@gmail.com>
2023-03-01 16:38:13 +0100

    wip on convert merging peft weights and extracting base model

--- examples/sentiment/load_cm_finetune_peft.py
+++ examples/sentiment/load_cm_finetune_peft.py
@@ -1,3 +1,5 @@
+import torch
+import peft
 from peft import LoraConfig, get_peft_model, prepare_model_for_int8_training
 from peft import PeftModelForCausalLM, PeftModel
 from transformers import pipeline, AutoTokenizer, HfArgumentParser, AutoModelForCausalLM
@@ -10,11 +12,24 @@ from transformers import pipeline, AutoTokenizer, HfArgumentParser, AutoModelFor
 #     bias="none",
 #     task_type="CAUSAL_LM",
 # )
-pretrained_model = AutoModelForCausalLM.from_pretrained("edbeeching/gpt-neo-125M-imdb", load_in_8bit=True, device_map="auto")
-model = PeftModel.from_pretrained(
-    pretrained_model,
-    "edbeeching/gpt-neo-125M-imdb-ppo-sentiment",
-    load_in_8bit=True,
-    device_map="auto")
 
-print(model)
+model = AutoModelForCausalLM.from_pretrained("edbeeching/gpt-neox-20b-imdb-peft-adapter-removed", load_in_8bit=True, device_map="auto")
+
+
+# converting the model
+# pretrained_model = AutoModelForCausalLM.from_pretrained("EleutherAI/gpt-neox-20b", torch_dtype=torch.float16)
+# model = PeftModel.from_pretrained(
+#     pretrained_model,
+#     "edbeeching/gpt-neox-20b-imdb-imdb-peft")
+
+# model.eval()
+
+# key_list = [key for key, _ in model.base_model.model.named_modules() if "lora" not in key]
+# for key in key_list:
+#     parent, target, target_name = model.base_model._get_submodules(key)
+#     if isinstance(target, peft.tuners.lora.Linear):
+#         bias = target.bias is not None
+#         new_module = torch.nn.Linear(target.in_features, target.out_features, bias=bias)
+#         model.base_model._replace_module(parent, target_name, new_module, target)
+
+# print(model)
